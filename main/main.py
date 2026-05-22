aa085bcd934fc9f76c83aba4236288e5f436a2c9
Aarav Agarwal <279409447+python-neo@users.noreply.github.com>
2026-05-22 11:44:22 +0530

    Added file loading and saving

--- main/main.py
+++ main/main.py
@@ -1,20 +1,31 @@
+from argparse import ArgumentParser
+from datetime import datetime
 from json import dump, load
 from json import JSONDecodeError
 from pathlib import Path
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, TextArea
+from textual.containers import Horizontal
+from textual.widgets import Header, Label, TextArea
+import sys
+
+BASE_DIR = Path (__file__).resolve ().parent
 
 class Kable (App) :
-    CSS = """
-    TextArea {
-        height: 1fr;
-    }
-    """
+    CSS = (BASE_DIR / "styles.txt").read_text ()
+    BINDINGS = [("ctrl+s", "save", "Save")]
 
-    def __init__ (self) -> None :
+    def __init__ (self, file_path : Path | None = None) -> None :
         super ().__init__ ()
-        self.config_path = Path (__file__).resolve ().parent / "config.json"
+        self.config_path = (BASE_DIR / "config.json")
         self.config : dict = {}
+        self.current_file = file_path
+        self.initial_text = ""
+
+        if self.current_file is not None :
+            try :
+                self.initial_text = self.current_file.read_text (encoding = "utf-8")
+            except (OSError, UnicodeError) :
+                sys.exit ("File not found or cannot be read.")
 
         if self.config_path.exists () :
             try :
@@ -27,6 +38,8 @@ class Kable (App) :
 
     def on_mount (self) -> None :
         self.theme = self.config.get ("theme", "textual-dark")
+        self.update_clock ()
+        self.set_interval (1, self.update_clock)
 
     def _on_exit_app (self) :
         self.config ["theme"] = self.theme
@@ -37,10 +50,28 @@ class Kable (App) :
         with self.config_path.open ("w", encoding = "utf-8") as f :
             dump (self.config, f, indent = 4)
 
+    def action_save (self) -> None :
+        if self.current_file is None :
+            return
+        editor = self.query_one (TextArea)
+        self.current_file.write_text (editor.text, encoding = "utf-8")
+
+    def update_clock (self) -> None :
+        self.query_one ("#status_clock", Label).update (
+            f"🕒 {datetime.now ().strftime ('%H:%M')}"
+        )
+
     def compose (self) -> ComposeResult :
         yield Header ()
-        yield TextArea ()
-        yield Footer ()
+        yield TextArea (self.initial_text)
+        with Horizontal (id = "status_bar") :
+            yield Label ("", classes = "spacer")
+            yield Label ("00:00", id = "status_clock")
 
 if __name__ == "__main__" :
-    Kable ().run ()
+    argparser = ArgumentParser ()
+    argparser.add_argument ("filename")
+    args = argparser.parse_args ()
+
+    file_path = Path (args.filename).resolve ()
+    Kable (file_path = file_path).run ()
